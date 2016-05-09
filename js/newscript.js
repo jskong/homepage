8650b111a2e7da8540f15426b429bc06d8a4dc23
Jillian Kong <jillian.kong@alumni.ubc.ca>
2016-05-09 12:06:26 -0600

    (hopefully) change content?
    supposedly change tab content.

--- js/newscript.js
+++ js/newscript.js
@@ -2,57 +2,57 @@ function changeTarget(tabNum) {
 	// reset selection
 	for (i=0; i<4; i++) {
 		document.getElementById('connector' + i).style.visibility = 'hidden';
-		document.getElementById('button' + i).className = 
+		document.getElementById('button' + i).className =
 			document.getElementById('button' + i).className.replace( /(?:^|\s)selected(?!\S)/ , '' );
 	}
-	
+
 	// a wild textbox appears!
 	document.getElementById('target').style.display = 'block';
-	
+
 	// make button 'selected'
 	document.getElementById('button' + tabNum).className += ' selected';
-	
+
 	// make connector visible
 	document.getElementById('connector' + tabNum).style.visibility = 'visible';
-	
-	// change heading
-	var head = document.getElementById('targetHead');
-	var tabName = document.getElementById('button' + tabNum).innerHTML;
-	head.innerHTML = tabName;
-	
+
+	// change content
+	var target = document.getElementById('target');
+	var content = document.getElementById('tab' + tabNum).innerHTML;
+	target.innerHTML = content;
+
 	return;
-	
+
 }
 
 function fadeOut(elemID) {
     var elem = document.getElementById(elemID);
     var opacity = 1.0;
     var id = setInterval(frame, 6);
-    
+
     function frame() {
         if (opacity == 0.0) {
             clearInterval(id);
         } else {
-            opacity -= 0.05; 
-            elem.style.opacity = opacity; 
+            opacity -= 0.05;
+            elem.style.opacity = opacity;
         }
-    } 
+    }
     return;
 }
 
 function fadeIn(elemID) {
     var elem = document.getElementById(elemID);
-    var opacity = 0.0; 
+    var opacity = 0.0;
     var id = setInterval(frame, 6);
-    
+
     function frame() {
         if (opacity == 1.0) {
             clearInterval(id);
         } else {
-            opacity += 0.05; 
-            elem.style.opacity = opacity; 
+            opacity += 0.05;
+            elem.style.opacity = opacity;
         }
-    } 
+    }
     return;
 }
 /*
@@ -67,9 +67,9 @@ function test(pre,expected) {
 		failures++;
 		return;
 	} else changeTarget(expected);
-	
+
 	var actual = document.getElementById('targetHead').innerHTML;
-	
+
 	if (actual != expected) {
 		console.log('Expected ' + expected + ' but produced ' + actual + ' .');
 		failures++;
